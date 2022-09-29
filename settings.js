eb3f424d18ef926bba00fdedee0de242a3f17d97
lakshan01 <110474040+lakshan01@users.noreply.github.com>
2022-09-29 14:35:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94714188730'] //ur owner number
-global.ownername = "nipuna rangana" //ur owner name
-global.ytname = "YT: 𝐂𝐈𝐁𝐄𝐑 𝐓𝐄𝐂𝐇" //ur yt chanel name
-global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
+global.vcardowner = ['94761676948'] //ur owner number
+global.ownername = "🔖𝗠𝗥.𝗔𝗥𝗢𝗦𝗛𝗔" //ur owner name
+global.ytname = "YT:🔖𝗠𝗥.𝗔𝗥𝗢𝗦𝗛𝗔" //ur yt chanel name
+global.socialm = "GitHub: 🔖𝗠𝗥.𝗔𝗥𝗢𝗦𝗛𝗔" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['94714188730']
-global.ownertag = '94714188730' //ur tag number
-global.botname = '📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶' //ur bot name
+global.owner = ['94761676948']
+global.ownertag = '94761676948' //ur tag number
+global.botname = '👻𝗔𝗥𝗢𝗦𝗛𝗔💚𝗕𝗢𝗧👻' //ur bot name
 global.linkz = "https://youtu.be/3J9-8U8St3U" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/3J9-8U8St3U" //ur website to be displayed
 global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark packname
-global.author = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark author
+global.packname = "👻𝗔𝗥𝗢𝗦𝗛𝗔💚𝗕𝗢𝗧👻" //ur sticker watermark packname
+global.author = "👻𝗔𝗥𝗢𝗦𝗛𝗔💚𝗕𝗢𝗧👻" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['94714188730'] //ur premium numbers
+global.premium = ['94761676948'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
